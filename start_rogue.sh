b595ad2d3347eea2e55c772d80308eb1623f5a0e
M7hir <mihirmdg123@gmail.com>
2026-03-09 13:56:25 -0300

    changes start_rouge

--- start_rogue.sh
+++ start_rogue.sh
@@ -11,6 +11,10 @@ sudo python3 run.py --node S4 --cmd "sudo /usr/sbin/bgpd -f conf/bgpd-S4.conf -d
 PID2=$!
 wait $PID1 $PID2
 
+# Configure S4 interface to communicate with the attacker host (h4-1)
+# This is necessary because h4-1 is on 14.0.1.0/24, but zebra config might not set this up.
+sudo python3 run.py --node S4 --cmd "sudo ifconfig S4-eth1 14.0.1.254 netmask 255.255.255.0 up"
+
 # Add a NAT rule on the rogue router to forward hijacked traffic to the attacker's webserver.
 # Hijacked traffic for 13.0.1.1 will be redirected to h4-1 (which now has IP 14.0.1.1).
 echo "Adding NAT rule on S4 to redirect traffic"
